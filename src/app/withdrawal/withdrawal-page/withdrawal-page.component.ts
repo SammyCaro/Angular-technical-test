0b8223d9215fd94b9507629f68e9ed2b867e8a44
SammyCaro <sammy.caro94@gmail.com>
2022-10-18 14:34:00 -0300

    adding html, fonts and general styles

--- src/app/withdrawal/withdrawal-page/withdrawal-page.component.ts
+++ src/app/withdrawal/withdrawal-page/withdrawal-page.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-withdrawal-page',
   templateUrl: './withdrawal-page.component.html',
-  styleUrls: ['./withdrawal-page.component.css']
+  styleUrls: ['./withdrawal-page.component.css', '../../app.component.css'],
 })
 export class WithdrawalPageComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
